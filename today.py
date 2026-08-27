4695cdbfb2dd8de6179c94571ead5d548782a12b
kalis26 <aminera2006@gmail.com>
2026-08-27 16:09:31 +0100

    fix: attempt to fix parsing

--- today.py
+++ today.py
@@ -331,15 +331,15 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'age_data', age_data)
-    justify_format(root, 'commit_data', commit_data, 33)
+    justify_format(root, 'age_data', age_data, 59)
+    justify_format(root, 'commit_data', commit_data, 34)
     justify_format(root, 'star_data', star_data, 47)
     justify_format(root, 'repo_data', repo_data, 13)
     justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'follower_data', follower_data, 40)
-    justify_format(root, 'loc_data', loc_data[2], 9)
+    justify_format(root, 'follower_data', follower_data, 39)
+    justify_format(root, 'loc_data', loc_data[2], 22)
     justify_format(root, 'loc_add', loc_data[0], 22)
-    justify_format(root, 'loc_del', loc_data[1], 7)
+    justify_format(root, 'loc_del', loc_data[1], 22)
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 
